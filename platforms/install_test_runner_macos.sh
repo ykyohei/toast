a2212a97b30c0bb106b3567df9630a09c99f00a2
Theodore Kisner <tskisner.public@gmail.com>
2022-06-13 11:28:17 -0700

    Update compile flags for macos wheels

--- platforms/install_test_runner_macos.sh
+++ platforms/install_test_runner_macos.sh
@@ -25,7 +25,7 @@ export TOAST_BUILD_CMAKE_C_COMPILER=clang
 export TOAST_BUILD_CMAKE_CXX_COMPILER=clang++
 export TOAST_BUILD_BLA_VENDOR='Apple'
 export TOAST_BUILD_DISABLE_OPENMP=1
-export TOAST_BUILD_CMAKE_CXX_FLAGS="-O2 -g -fPIC"
+export TOAST_BUILD_CMAKE_C_FLAGS="-O2 -g -fPIC"
 export TOAST_BUILD_CMAKE_CXX_FLAGS="-O2 -g -fPIC -std=c++11 -stdlib=libc++"
 export TOAST_BUILD_CMAKE_VERBOSE_MAKEFILE=ON
 python3 -m pip -vvv install .
